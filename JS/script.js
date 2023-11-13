2e552053feb8a6e1a104f90e4e38f940521883d9
tobrien9 <143633513+tobrien9@users.noreply.github.com>
2023-11-12 21:15:26 -0600

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,13 @@
+function init(){
+  var button = document.getElementById('entrybutton');
 
+  function showMeText(){
+    var textbox = document.getElementById('entryinput');
+    document.getElementById('textoutput').innerHTML = textbox.value;
+    alert("Timothy O'Brien: " + textbox.value);
+  }
+
+  button.addEventListener('click', showMeText);
+  }
+  
+window.addEventListener('load', init);
